1c8f6ba72cab7d8bb377ba2fc96e4e09eb458352
BorisZ-git <ribos@bk.ru>
2017-03-25 10:21:57 +0300

    start Task 2\\ create form

---
+++ HomeWork/HomeWork2/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/* Boris Z
+ * Используя Windows Forms, разработать игру “Угадай число”.
+ * Компьютер загадывает число от 1 до 100, 
+ * а человек пытается его угадать за минимальное число попыток.
+ * Для ввода данных от человека используется элемент TextBox.*/
+
+namespace HomeWork2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
