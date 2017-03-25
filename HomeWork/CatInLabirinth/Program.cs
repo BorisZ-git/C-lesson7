10ee29755654bbf86078e17bc38358c22cd0424f
BorisZ-git <ribos@bk.ru>
2017-03-25 11:24:48 +0300

    start Task 3\\ Create form

---
+++ HomeWork/CatInLabirinth/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Boris Z
+ * Котик в лабиринте:
+ * Есть точка начала(котик)
+ * Есть точка выхода из лабиринта
+ * Есть препятствия
+ * Котик может двигаться только вверх-вниз
+ * Найти кратчайший путь
+*/
+
+namespace CatInLabirinth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ HomeWork/HomeWork3/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/* Boris Z
+ * * Реализовать программу из предыдущего урока с шаблоном документа на отпуск в Windows
+Forms. Сделать несколько текстовых полей(TextBox), куда человек вводит данные, а по нажатии
+кнопки “Сделать” ­ видит готовое заявление на отпуск.*/
+
+namespace HomeWork3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
