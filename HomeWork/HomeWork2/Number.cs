97e4ed53d0bf0be285a1ef25e77fa93eb71f4083
BorisZ-git <ribos@bk.ru>
2017-03-25 10:42:45 +0300

    create class number contain info about number and method make a number and method that compare user answer and number

---
+++ HomeWork/HomeWork2/Number.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork2
+{
+    class Number
+    {
+        public static int num;
+        public static void MaNumber()
+        {
+            Random r = new Random();
+            num = r.Next(0,100);
+            MessageBox.Show("компьютер загадал число");
+        }
+        public static void CheckAnswer(string UserAnswer)
+        {
+            int usernum = 0;
+            try
+            {
+                usernum =  Convert.ToInt32(UserAnswer);
+            }
+            catch
+            {
+                MessageBox.Show("нужно ввести число");
+            }
+            if (usernum == num)
+                MessageBox.Show("Продолжить или выйти", "победа", MessageBoxButtons.YesNo);
+            else if (usernum > num)
+                MessageBox.Show("перебор");
+            else if (usernum < num)
+                MessageBox.Show("недобор");
+        }
+    }
+}
